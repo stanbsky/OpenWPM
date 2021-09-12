125fc2f7b5801d21ce9017f2e859f06beca5fdd0
kampanosg <kampanosg@outlook.com>
2021-09-12 18:46:35 +0100

    gk - added placeholder for accept cookie command

--- openwpm/commands/browser_commands.py
+++ openwpm/commands/browser_commands.py
@@ -518,3 +518,22 @@ class InitializeCommand(BaseCommand):
 
         msg = {"action": "Initialize", "visit_id": self.visit_id}
         extension_socket.send(msg)
+
+
+class AcceptCookieNoticeCommand(BaseCommand):
+
+    def __init__(self, sleep):
+        self.sleep = sleep
+
+    def __repr__(self):
+        return f"AcceptCookieNoticeCommand({self.sleep})"
+
+    def execute(
+        self,
+        webdriver,
+        browser_params,
+        manager_params,
+        extension_socket,
+    ):
+
+        logger.info('I will accept the cookie banner!')
