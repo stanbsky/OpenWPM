0377a621bb30deb3fc4935fae41b648f54710440
George Kampanos <30287348+kampanosg@users.noreply.github.com>
2021-12-20 15:00:40 +0000

    two sep crawls

--- demo.py
+++ demo.py
@@ -14,19 +14,7 @@ from idcac_cookie_selectors import get_idcac_css_selectors
 
 # The list of sites that we wish to crawl
 NUM_BROWSERS = 1
-sites = [
-    'https://gov.uk/',
-    'https://gov.scot/',
-    'https://gov.wales',
-    'https://aib.gov.uk',
-    'https://census.gov.uk/',
-    'https://ccrc.gov.uk',
-    'https://cps.gov.uk',
-    'https://data.gov.uk',
-    'https://justice.gov.uk',
-    'https://tfw.wales',
-    'https://gpa.gov.uk/'
-    ]
+sites = ['https://www.audit-scotland.gov.uk']
 
 # f = open("sites.txt", "r")
 # sites = f.read().split('\n')
@@ -39,7 +27,7 @@ browser_params = [BrowserParams(display_mode="headless") for _ in range(NUM_BROW
 
 # Update browser configuration (use this for per-browser settings)
 for browser_param in browser_params:
-    # Record HTTP Requests and Responses
+    # Record HTTP Requests and Ressponses
     browser_param.http_instrument = True
     # Record cookie changes
     browser_param.cookie_instrument = True
@@ -51,8 +39,6 @@ for browser_param in browser_params:
     browser_param.callstack_instrument = True
     # Record DNS resolution
     browser_param.dns_instrument = True
-    # Dump profile info
-    browser_param.custom_params = {'dump_profile': True, 'profile_dir': './datadir/'}
 
 # Update TaskManager configuration (use this for crawl-wide settings)
 manager_params.data_directory = Path("./datadir/")
@@ -68,7 +54,38 @@ manager_params.log_path = Path("./datadir/openwpm.log")
 with TaskManager(
     manager_params,
     browser_params,
-    SQLiteStorageProvider(Path("./datadir/crawl-data.sqlite")),
+    SQLiteStorageProvider(Path("./datadir/crawl-data-pre.sqlite")),
+    None,
+) as manager:
+
+    # Visits the sites
+    for index, site in enumerate(sites):
+
+        def callback(success: bool, val: str = site) -> None:
+            print(
+                f"CommandSequence for {val} ran {'successfully' if success else 'unsuccessfully'}"
+            )
+
+        # Parallelize sites over all number of browsers set above.
+        command_sequence = CommandSequence(
+            site,
+            site_rank=index,
+            callback=callback,
+        )
+
+        # Start by visiting the page
+        command_sequence.append_command(GetCommand(url=site, sleep=6), timeout=60)
+
+        command_sequence.append_command(CollectCookiesCommand(stage='pre-accept'))
+
+        # Run commands across all browsers (simple parallelization)
+        manager.execute_command_sequence(command_sequence)
+
+# Commands time out by default after 60 seconds
+with TaskManager(
+    manager_params,
+    browser_params,
+    SQLiteStorageProvider(Path("./datadir/crawl-data-post.sqlite")),
     None,
 ) as manager:
 
@@ -87,13 +104,10 @@ with TaskManager(
             site,
             site_rank=index,
             callback=callback,
-            reset=True
         )
 
         # Start by visiting the page
         command_sequence.append_command(GetCommand(url=site, sleep=6), timeout=60)
-
-        command_sequence.append_command(CollectCookiesCommand(stage='pre-accept'))
 
         command_sequence.append_command(AcceptCookiesCommand(css_selectors=css_selectors), timeout=60)
 
